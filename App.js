01a639a22df578820041763dd2f8abe0929cc254
Rafael Augusto <103936985+rafael-tecinternet@users.noreply.github.com>
2023-01-19 11:52:35 -0300

    mapview com localização atual

--- App.js
+++ App.js
@@ -1,8 +1,32 @@
-import { useState } from "react";
-import { StyleSheet, Text, View, StatusBar, Image } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, StatusBar, Alert, Button } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function App() {
+  /* State para a geolocalização */
+  const [minhaLocalizacao, setMinhaLocalizacao] = useState(null);
+
+  useEffect(() => {
+    async function obterLocalizacao() {
+      /* Acessando o status da requisição de permissão de uso */
+      const { status } = Location.requestForegroundPermissionsAsync();
+      /* Verificando o status */
+      /*    if (status !== "granted") {
+        Alert.alert(
+          "Ops!",
+          "Você não autorizou o uso de recursos de localização"
+        );
+        return;
+      } */
+      /* Acessando os dados de geolocalização */
+      let localizacaoAtual = await Location.getCurrentPositionAsync({});
+      /* Adicionando os dados ao state */
+      setMinhaLocalizacao(localizacaoAtual);
+    }
+    obterLocalizacao();
+  }, []);
+  console.log(minhaLocalizacao);
   const regiaoInicial = {
     latitude: -23.533773,
     longitude: -46.65529,
@@ -15,8 +39,8 @@ export default function App() {
     setLocalizacao({
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
-      latitude: event.nativeEvent.coordinate.latitude,
-      longitude: event.nativeEvent.coordinate.longitude,
+      latitude: minhaLocalizacao.coords.latitude,
+      longitude: minhaLocalizacao.coords.longitude,
     });
     console.log(localizacao);
   };
@@ -25,27 +49,33 @@ export default function App() {
     <>
       <StatusBar />
       <View style={estilos.container}>
-        <MapView
-          onPress={marcarLocal}
-          style={estilos.mapa}
-          region={localizacao ?? regiaoInicial}
-          liteMode={false} //somente android
-          mapType="hybrid"
-        >
-          {localizacao && (
-            <Marker
-              coordinate={localizacao}
-              title="Aqui"
-              onPress={(event) => console.log(event.nativeEvent)}
-            />
-          )}
-        </MapView>
+        <View style={estilos.botao}>
+          <Button title="Onde estou?" onPress={marcarLocal} />
+        </View>
+        <View style={estilos.viewMapa}>
+          <MapView
+            onPress={marcarLocal}
+            style={estilos.mapa}
+            region={localizacao ?? regiaoInicial}
+            liteMode={false} //somente android
+            mapType="hybrid"
+          >
+            {localizacao && (
+              <Marker
+                coordinate={localizacao}
+                title="Aqui"
+                onPress={(event) => console.log(event.nativeEvent)}
+              />
+            )}
+          </MapView>
+        </View>
       </View>
     </>
   );
 }
 
 const estilos = StyleSheet.create({
+  viewMapa: { flex: 1 },
   mapa: {
     width: "100%",
     height: "100%",
